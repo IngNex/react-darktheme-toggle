11f6b9051bcebdb962c5c0d6557c10e71784e694
IngNex <ingnexmaicol14@gmail.com>
2022-09-17 14:25:17 -0500

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import MoonIcon from "./components/icons/MoonIcon";
+import SunIcon from "./components/icons/SunIcon";
+import Switch from "./components/Switch";
+
+const style = {
+  minHeight: "100vh",
+  textAlign: "center",
+  paddingTop: "10rem",
+  backgroundColor: "#f5f5f5",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={style}>
+      <SunIcon />
+      <Switch />
+      <MoonIcon />
+      <h1>Jim Naysium</h1>
+      <p>Username: Michael Rodriguez</p>
+      <p>Email: ingnexmaicol14@gmail.com</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
