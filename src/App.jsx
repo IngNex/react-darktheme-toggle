5bc536b6432e05707bf29cc8cff390eb7ca312b8
IngNex <ingnexmaicol14@gmail.com>
2022-09-17 14:47:31 -0500

    update app.jsx light and dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import MoonIcon from "./components/icons/MoonIcon";
 import SunIcon from "./components/icons/SunIcon";
 import Switch from "./components/Switch";
@@ -8,19 +9,43 @@ const StyledApp = styled.div`
   min-height: 100vh;
   text-align: center;
   padding-top: 10rem;
-  background-color: #f5f5f5;
+  background-color: ${(props)=> props.theme.body};
 `
+const Name = styled.h1`
+  margin: 1rem;
+  color: ${(props) => props.theme.title};
+`
+const Info = styled.p`
+  margin: 1rem;
+  color: ${(props) => props.theme.subtitle};
+`
+const darkTheme = {
+  body: "#1c1c1c",
+  title: "#fff",
+  subtitle: "#b6b6b6",
+}
+const lightTheme = {
+  body: "#fff",
+  title: "#000",
+  subtitle: "#000",
+}
 
 function App() {
+
+  const [theme, useTheme] = useState("light");
+  const isDarkTheme = theme === "light";
+
   return (
-    <StyledApp>
-      <SunIcon />
-      <Switch />
-      <MoonIcon />
-      <h1>Michael Rodriguez</h1>
-      <p>Username: IngNex</p>
-      <p>Email: ingnexmaicol14@gmail.com</p>
-    </StyledApp>
+    <ThemeProvider theme={lightTheme}>
+      <StyledApp>
+        <SunIcon />
+        <Switch />
+        <MoonIcon />
+        <Name color="red">Michael Rodriguez</Name>
+        <Info>Username: IngNex</Info>
+        <Info>Email: ingnexmaicol14@gmail.com</Info>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
